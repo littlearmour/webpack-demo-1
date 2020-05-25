6533efb13e4ce4e0291f0c3089bf1d145a3df9e6
littlearmour <1375524732@qq.com>
2020-05-25 11:25:33 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "fan",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
